004f9b669805f228a65c37e87656d4c216f2d8c9
joydeep-roy7 <royjoydeep906@gmail.com>
2026-06-16 13:55:25 +0530

    add profile controller

---
+++ lib/controllers/profile_controller.dart
@@ -0,0 +1,19 @@
+
+import 'package:expense_tracker/data/models/profile_models.dart';
+import 'package:get/get.dart';
+
+class ProfileController extends GetxController {
+
+  ProfileModels? profile;
+
+  void addProfile(ProfileModels newProfile) {
+    profile = newProfile;
+    update();
+  }
+
+  void updateProfile(ProfileModels newProfile) {
+    profile = newProfile;
+    update();
+  }
+
+}
